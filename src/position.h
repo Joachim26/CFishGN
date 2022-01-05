7f04d30977e28c7d126dd7aee188b5b5a8ee541a
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2022-01-05 17:44:30 +0100

    Update to "Remove lowPly history" https://github.com/official-stockfish/Stockfish/commit/dc5d9bdfee70f4267d9a49ad71e5ee478dd50ca5

--- src/position.h
+++ src/position.h
@@ -180,7 +180,6 @@ struct Position {
   // Pointers to thread-specific tables.
   CounterMoveStat *counterMoves;
   ButterflyHistory *mainHistory;
-  LowPlyHistory *lowPlyHistory;
   CapturePieceToHistory *captureHistory;
   PawnEntry *pawnTable;
   MaterialEntry *materialTable;
